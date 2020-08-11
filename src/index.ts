6321004d8959917018c09d95c03c5f327ae90346
andreasgarvik <andreas.garvik13@gmail.com>
2020-08-11 09:30:34 +0200

    :sparkles:

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cookieSession from 'cookie-session'
-import { router } from './routes/loginRoutes'
+import { AppRouter } from './AppRouter'
+import './controllers/LoginController'
+import './controllers/RootController'
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieSession({ keys: ['123'] }))
-app.use(router)
+app.use(AppRouter.router)
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000')
